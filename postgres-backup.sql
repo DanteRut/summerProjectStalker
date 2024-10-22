7269c81b977ef591d7fa6eb007063c808512feee
DanteRut <152468589+DanteRut@users.noreply.github.com>
2024-10-22 19:07:31 +0300

    Add files via upload

--- postgres-backup.sql
+++ postgres-backup.sql
@@ -1,1 +1,1 @@
-OCI runtime exec failed: exec failed: unable to start container process: exec: "/usr/bin/pg_dump": stat /usr/bin/pg_dump: no such file or directory: unknown
+OCI runtime exec failed: exec failed: unable to start container process: exec: "C:\\Users\\ROOT": executable file not found in $PATH: unknown
